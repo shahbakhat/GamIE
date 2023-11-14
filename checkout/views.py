b2297b31274ef401bfb31beb5050d465e942f304
Shah Bakhat <78231325+shahbakhat@users.noreply.github.com>
2023-11-14 14:15:15 +0000

    created signals for checkout and forms for order as order forms

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404, HttpResponse
+)
+from django.contrib import messages
 # Create your views here.
+def checkout(request):
+    cart = request.session.get('cart',{})
+    if not cart:
+        message.error(request,"Youe bag is empty at the moment")
+        return redirect(reverse('products'))
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form
+    }
+    return render (request, template, context)
