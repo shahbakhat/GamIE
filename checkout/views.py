d1351ee3e627de06a0b2374324efdcf35d359d55
Shah Bakhat <78231325+shahbakhat@users.noreply.github.com>
2023-11-14 14:27:13 +0000

    removed env folder from tracking

--- checkout/views.py
+++ checkout/views.py
@@ -1,16 +1,20 @@
+from django.contrib import messages
 from django.shortcuts import (
     render, redirect, reverse, get_object_or_404, HttpResponse
 )
-from django.contrib import messages
+
+
+from django.conf import settings
+
 # Create your views here.
 def checkout(request):
     cart = request.session.get('cart',{})
     if not cart:
-        message.error(request,"Youe bag is empty at the moment")
+        messages.error(request, "Your bag is empty at the moment")  # Fix the typo here
         return redirect(reverse('products'))
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form
     }
-    return render (request, template, context)
+    return render(request, template, context)
--- gamie/urls.py
+++ gamie/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
+    path('checkout/', include('checkout.urls')),
 ]
 
 if settings.DEBUG:
