8aef35e174d6588a6148682254ccb7b799147974
Shah Bakhat <78231325+shahbakhat@users.noreply.github.com>
2023-11-14 10:33:58 +0000

    added product detail and implemented search for products

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.db.models import Q
+from django.contrib import messages
 from .models import Product
 # Create your views here.
 
@@ -6,9 +8,32 @@ def all_products (request):
     """A view to render all products page"""
 
     products = Product.objects.all()
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You have not enttered any search criteria!") 
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query) 
+            products = products.filter(queries)
+
 
     context = {
         'products': products,
     }
 
     return render(request, 'products/products.html', context)
+
+def product_detail(request, product_id):
+    """
+    A view to show individual product details
+    """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
