232f6a5c95398a5cc2b6b421d6421c54c4ba49ab
Shah Bakhat <78231325+shahbakhat@users.noreply.github.com>
2023-11-14 08:26:48 +0000

    Customized admin for spelling correction

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,31 @@
 from django.contrib import admin
 from . models import Product,Category
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Custom admin Product Model.
+    """
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Custom admin Category Model
+    """
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
