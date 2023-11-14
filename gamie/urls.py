3cab91863f3dce17fa89f01fd363a9a5bbe07ae4
Shah Bakhat <78231325+shahbakhat@users.noreply.github.com>
2023-11-14 20:25:52 +0000

    deleted the cart app

--- gamie/urls.py
+++ gamie/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
-    path('cart/', include('cart.urls')),
+    path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 ]
